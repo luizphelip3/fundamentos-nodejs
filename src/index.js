0d7eb62286eeda95358ceb581ef6e3bc062476d3
luizphelip3 <luiz_phelip3@hotmail.com>
2022-06-06 14:30:30 -0300

    feat: added middleware .json() into project and builded some simple methods

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 
-const app = express();
+const app = express();  
+
+
+// essa função é um middleware do próprio express que serve para 
+// permitir receber dados em formato .json em requisições
+app.use(express.json())
 
 app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
   return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (req, res) => {
+  const body = req.body;
+  console.log(body)
   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
   return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
